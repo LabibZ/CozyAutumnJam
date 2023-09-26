df64077e9ee0ade6bbba44b60bfd32373e89d834
Labib <labib619@hotmail.com>
2023-09-26 19:26:39 -0400

    minor fixes to machine timer

--- Player/PlayerScript.gd
+++ Player/PlayerScript.gd
@@ -6,7 +6,7 @@ const SPEED = 100
 var all_interactions = []
 
 @onready var animationPlayer = $AnimationPlayer
-@onready var interactableFinder = $Marker2D/InteractableFinder
+@onready var interactableFinder = $Direction/InteractableFinder
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -27,25 +27,8 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("interact"):
 		execute_interaction()
 
-#func check_nearest_interactable(nearest_interactable) -> void:
-#	var areas: Array[Area2D] = interactableFinder.get_overlapping_areas()
-#	var shortest_distance: float = INF
-#	var next_nearest_interactable: Interactable = null
-#	for area in areas:
-#		var distance: float = area.global_position.distance_to(global_position)
-#		if distance < shortest_distance:
-#			shortest_distance = distance
-#			next_nearest_interactable = area
-#	if next_nearest_interactable != nearest_interactable or not is_instance_valid(next_nearest_interactable):
-#		nearest_interactable = next_nearest_interactable
-#		emit_signal("nearest_actionable_changed", nearest_interactable)
-#	print("interactable")
-#	print(nearest_interactable)
-
-
 func _on_interactable_finder_area_entered(area):
 	all_interactions.push_back(area)
-
 
 func _on_interactable_finder_area_exited(area):
 	all_interactions.erase(area)
--- Scripts/Machine.gd
+++ Scripts/Machine.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Interactable
 
 class_name Machine
 
@@ -11,7 +11,7 @@ enum MachineState {
 var current_state: MachineState = MachineState.IDLE
 var processing_time: float = 5.0
 var timer: Timer
-
+@onready var testTimer = $Time
 # Signals
 signal machine_done
 
@@ -20,18 +20,28 @@ func _ready():
 	add_child(timer)
 	timer.timeout.connect(processing_done)
 
-func start_processing() -> void:
+func _process(delta):
+	testTimer.text = "%0.2f" % timer.time_left
+
+func interact():
 	if current_state == MachineState.IDLE:
-		current_state = MachineState.WORKING
-		timer.start(processing_time)
-		print("working")
+		start_processing()
+	elif current_state == MachineState.DONE:
+		reset_machine()
+
+func start_processing() -> void:
+	current_state = MachineState.WORKING
+	timer.start(processing_time)
+	print("working")
 
 func processing_done() -> void:
 	current_state = MachineState.DONE
-	emit_signal("machine_done")
+	# emit_signal("machine_done")
+	timer.stop()
 	print("done")
 	
 func reset_machine() -> void:
 	current_state = MachineState.IDLE
+	print("idle")
 
 
