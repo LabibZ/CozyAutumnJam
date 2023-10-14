228158cf3fe796ff846851090b81962ffe2b07d6
Jennifer Ruan <jenn@Jennifers-MacBook-Air.local>
2023-10-14 17:26:35 -0400

    added coffeemachine sfx

--- Scripts/Machines/CoffeeMachine.gd
+++ Scripts/Machines/CoffeeMachine.gd
@@ -1,15 +1,22 @@
 class_name CoffeeMachine extends Machine
 
+@onready var sound = $AudioStreamPlayer
+
 func check_requirements() -> bool:
 	if item is CoffeeCup:
 		item = item as CoffeeCup
 		if !item.filled:
 			return true
 	return false
+	
+func start_processing() -> void:
+	super()
+	sound.play()
 
 func processing_done():
 	super()
 	item.fill()
+	sound.stop()
 
 func reset_machine() -> void:
 	super()
